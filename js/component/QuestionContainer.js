a8691c747206a4fc534409e8d0e82deb23404064
Dung <nguyenanhdung191@gmail.com>
2017-06-08 16:51:34 +0700

    push

--- js/component/QuestionContainer.js
+++ js/component/QuestionContainer.js
@@ -4,6 +4,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Checkbox from 'material-ui/Checkbox';
 import Next from 'material-ui/svg-icons/av/fast-forward';
 import Previous from 'material-ui/svg-icons/av/fast-rewind';
+import Correct from 'material-ui/svg-icons/navigation/check';
+import Incorrect from 'material-ui/svg-icons/navigation/close';
 
 
 export default class QuestionContainer extends React.Component {
@@ -12,26 +14,46 @@ export default class QuestionContainer extends React.Component {
         this.state = {
             currentQuestion: 0,
             questions: A1.questions,
+            complete: false
         };
+        this.state.questions[0].selected = true;
         this.countDown(new Date().getTime() + A1.maxTime);
     };
 
     handleNextQuestion = () => {
+        let object = this.state;
+        let finish = false;
+        object.questions[this.state.currentQuestion].responses.forEach(response => {
+            if (response.checked === true) {
+                finish = true;
+            }
+        });
+        object.questions[this.state.currentQuestion].finish = finish;
         let currentQuestionNo = this.state.currentQuestion;
         if (this.state.questions[currentQuestionNo + 1] !== undefined) {
-            this.setState({
-                currentQuestion: currentQuestionNo + 1
-            });
+            object.questions[currentQuestionNo].selected = false;
+            object.questions[currentQuestionNo + 1].selected = true;
+            object.currentQuestion += 1;
         }
+        this.setState(object);
     };
 
     handlePreviousQuestion = () => {
+        let object = this.state;
+        let finish = false;
+        object.questions[this.state.currentQuestion].responses.forEach(response => {
+            if (response.checked === true) {
+                finish = true;
+            }
+        });
+        object.questions[this.state.currentQuestion].finish = finish;
         let currentQuestionNo = this.state.currentQuestion;
         if (this.state.questions[currentQuestionNo - 1] !== undefined) {
-            this.setState({
-                currentQuestion: currentQuestionNo - 1
-            });
+            object.questions[currentQuestionNo].selected = false;
+            object.questions[currentQuestionNo - 1].selected = true;
+            object.currentQuestion -= 1;
         }
+        this.setState(object);
     };
 
     handleCheckResponse = (event, isInputChecked) => {
@@ -41,15 +63,25 @@ export default class QuestionContainer extends React.Component {
     };
 
     handleSelectQuestion = (event) => {
-        this.setState({
-            currentQuestion: parseInt(event.currentTarget.value)
-        })
+        let object = this.state;
+        let finish = false;
+        object.questions[this.state.currentQuestion].responses.forEach(response => {
+            if (response.checked === true) {
+                finish = true;
+            }
+        });
+        object.questions[this.state.currentQuestion].finish = finish;
+        object.questions[this.state.currentQuestion].selected = false;
+        object.questions[parseInt(event.currentTarget.value)].selected = true;
+        object.currentQuestion = parseInt(event.currentTarget.value);
+        this.setState(object);
     };
 
     handleComplete = () => {
         let correct = 0;
-        this.state.questions.forEach((question, questionIndex) => {
-            let correctAnswer = question.answer.sort((a, b) => a - b);
+        let object = this.state;
+        object.questions.forEach((question, questionIndex) => {
+            let correctAnswer = question.correctAnswer.sort((a, b) => a - b);
             let answer = [];
             question.responses.forEach((response, responseIndex) => {
                 if (response.checked === true) {
@@ -57,21 +89,31 @@ export default class QuestionContainer extends React.Component {
                 }
             });
             answer = answer.sort((a, b) => a - b);
+            object.questions[questionIndex].answer = answer;
             if (correctAnswer.toString() === answer.toString()) {
                 correct += 1;
+                object.questions[questionIndex].correct = true;
             }
         });
+        object.complete = true;
+        window.clearInterval(window.intervalId);
+        if (correct >= A1.minScore) {
+            $("#timer").html(`Chúc mừng, bạn đã thi đậu!!<br/>Số câu đúng: <strong>${correct} / ${A1.noOfQuestion}</strong>`);
+        } else {
+            $("#timer").html(`Rất tiếc, bạn đã thi rớt!!<br/>Số câu đúng: <strong>${correct} / ${A1.noOfQuestion}</strong>`);
+        }
+        this.setState(object);
     };
 
     countDown = (endTime) => {
-        let intervalId = setInterval(() => {
+        window.intervalId = setInterval(() => {
             let now = new Date().getTime();
             let minute = Math.floor((endTime - now) / 60000);
             let second = parseInt((((endTime - now) % 60000) / 1000).toFixed(0));
             $("#timer").html(`Thời gian còn lại: <strong>${(minute < 10) ? "0" + minute : minute} : ${(second < 10) ? "0" + second : second}</strong>`);
             if (minute <= 0 && second <= 0) {
-                alert("asdasdasdasdasdsad");
-                clearInterval(intervalId);
+                alert("Hết thời gian");
+                this.handleComplete();
             }
         }, 1000);
     };
@@ -82,15 +124,38 @@ export default class QuestionContainer extends React.Component {
                 <div className="question-selector">
                     {
                         this.state.questions.map((question, index) => {
+                            let backgroundColor = "";
+                            if (this.state.complete !== true) {
+                                if (question.finish === true) {
+                                    backgroundColor = "#00d80e";
+                                }
+                                else {
+                                    backgroundColor = "#e5e5e5";
+                                }
+                            } else {
+                                if (question.correct === true) {
+                                    backgroundColor = "#00d80e";
+                                } else {
+                                    backgroundColor = "#ff9696";
+                                }
+                            }
                             return (<RaisedButton
                                     key={index}
-                                    backgroundColor="#00d80e"
+                                    backgroundColor={backgroundColor}
                                     label={index + 1}
-                                    labelColor="#ffffff"
-                                    labelStyle={{fontWeight: "bold", fontSize: 20}}
-                                    style={{margin: 5}}
+                                    labelStyle={{
+                                        fontWeight: "bold",
+                                        fontSize: 20,
+                                        textDecoration: (question.selected === true) ? "underline" : null
+                                    }}
+                                    style={{margin: 5, borderWidth: 20}}
                                     value={index}
-                                    onClick={this.handleSelectQuestion}/>
+                                    onClick={this.handleSelectQuestion}
+                                    labelPosition="before"
+                                    icon={(this.state.complete === true) ? ((question.correct === true) ? <Correct/> :
+                                        <Incorrect/>) : null}/>
+
+
                             )
                         })
                     }
@@ -124,6 +189,43 @@ export default class QuestionContainer extends React.Component {
                 <div id="timer">Thời gian còn lại</div>
                 <div className="question">
                     Câu {this.state.currentQuestion + 1}: {this.state.questions[this.state.currentQuestion].question}</div>
+                <div className="question-answer">
+                    {
+                        (this.state.complete === true) ?
+                            <div>
+                                <div className="answer-status">
+                                    {
+                                        (this.state.questions[this.state.currentQuestion].correct) ?
+                                            <span style={{color: "#00ad22"}}>ĐÚNG!</span> :
+                                            <span style={{color: "#d10000"}}>SAI!</span>
+                                    }
+                                </div>
+                                <div>
+                                    {
+                                        <span>Đáp án:&nbsp;
+                                            {
+                                                this.state.questions[this.state.currentQuestion].correctAnswer.map(answer => {
+                                                    return answer + 1
+                                                }).toString()
+                                            }
+                                        </span>
+                                    }
+                                </div>
+                                <div>
+                                    {
+                                        <span>Câu trả lời của bạn:&nbsp;
+                                            {
+                                                this.state.questions[this.state.currentQuestion].answer.map(answer => {
+                                                    return answer + 1
+                                                }).toString()
+                                            }
+                                        </span>
+                                    }
+                                </div>
+                            </div>
+                            : ""
+                    }
+                </div>
                 <div className="question-picture">
                     {
                         this.state.questions[this.state.currentQuestion].hasOwnProperty("picture") ?
@@ -132,16 +234,18 @@ export default class QuestionContainer extends React.Component {
                     }
                 </div>
                 <div className="title">{this.state.questions[this.state.currentQuestion].title}</div>
-                <div className="responses">
+                < div className="responses">
                     {
                         this.state.questions[this.state.currentQuestion].responses.map((response, index) => {
                             return (
-                                <div key={index}>
+                                <div key={index} className="response-container">
                                     <Checkbox
-                                        label={`${index + 1}. ${response.text}`}
+                                        label={`${index + 1} - ${response.text}`}
                                         checked={this.state.questions[this.state.currentQuestion].responses[index].checked}
                                         onCheck={this.handleCheckResponse}
                                         id={`${this.state.currentQuestion}-${index}`}
+                                        disabled={this.state.complete}
+                                        labelStyle={{color: "#000000"}}
                                     />
                                 </div>
                             )
